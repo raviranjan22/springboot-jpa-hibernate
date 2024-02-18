9563a4e0dfdb8023eaedaa7da3a4a637b5436b73
Ravi Ranjan <raviraz73@gmail.com>
2024-02-18 07:47:05 +0530

    Fourth commit

--- learn-jpa-and-hibernate/src/main/java/com/ranjan/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/ranjan/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -4,16 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.ranjan.springboot.learnjpaandhibernate.course.Course;
+
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 	
 	@Autowired
 	private CourseJdbcRepository courseJdbcRepository;
 
+	/*
 	@Override
 	public void run(String... args) throws Exception {
 		courseJdbcRepository.insert();
 
+	}
+	*/
+	
+	@Override
+	public void run(String... args) throws Exception {
+		courseJdbcRepository.insert(new Course(1, "Learn JPA", "Ravi Ranjan"));
+		courseJdbcRepository.insert(new Course(2, "Learn Hibernate", "Ravi Ranjan"));
+		courseJdbcRepository.insert(new Course(3, "Learn JPA and Hibernate", "Ravi Ranjan"));
+		
+		courseJdbcRepository.deleteById(2);
 	}
 
 }
--- learn-jpa-and-hibernate/src/main/java/com/ranjan/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa-and-hibernate/src/main/java/com/ranjan/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.ranjan.springboot.learnjpaandhibernate.course.Course;
+
 @Repository
 public class CourseJdbcRepository {
 	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
+	/*
 	private static String INSERT_QUERY = 
 			"""
 				insert into course (id, name, author) values (1, 'Learn SpringBoot JPA and Hibernate', 'Ravi Ranjan');	
@@ -17,6 +20,26 @@ public class CourseJdbcRepository {
 	
 	public void insert() {
 		jdbcTemplate.update(INSERT_QUERY);
+	}
+	
+	*/
+	
+	private static String INSERT_QUERY = 
+			"""
+				insert into course (id, name, author) values (?, ?, ?);	
+			""";
+	
+	private static String DELETE_QUERY = 
+			"""
+				delete from course where id=?;	
+			""";
+	
+	public void insert(Course course) {
+		jdbcTemplate.update(INSERT_QUERY, course.getId(), course.getName(), course.getAuthor());
+	}
+	
+	public void deleteById(long id) {
+		jdbcTemplate.update(DELETE_QUERY, id);
 	}
 
 }
